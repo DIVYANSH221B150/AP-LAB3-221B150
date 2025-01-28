4fa0938f1a0b050de8de4b6e5821bf2790c44269
221B150 <221B150@juet.co.in>
2025-01-28 12:07:53 +0530

    Add problem 3 of LAB 1

---
+++ LAB 1/problem 3/case 3/Child.java
@@ -0,0 +1,6 @@
+class Child extends Mother {
+    @Override
+    public static void show() {
+        System.out.println("Child's show method");
+    }
+}
---
+++ LAB 1/problem 3/case 4/case4.java
@@ -0,0 +1,6 @@
+public class case4 {
+    public static void main(String[] args) {
+        Mother m1 = new Child();  
+        m1.show();  
+    }
+}
